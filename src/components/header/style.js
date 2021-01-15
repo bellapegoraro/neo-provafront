335795a69ca12709c14cda186ba4e91c637741b8
Isabella Pegoraro Marcondes <isabella.pegorarom@gmail.com>
2021-01-15 14:54:02 -0300

    feat: added footer component

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 
 const Container = styled.div`
   background-color: #8d99ae;
+  height: 55px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 1px 2px 5px 1px rgba(164, 164, 164, 0.75);
 `;
 
 export { Container };
--- src/components/search/style.js
+++ src/components/search/style.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  background-color: #edf2f4;
+  width: 250px;
+  height: 40px;
+  border-radius: 10px;
+`;
 
 export { Container };
